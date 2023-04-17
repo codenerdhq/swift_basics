f6b7c8ecab45f35249095d84a48193665cb7b7df
CodeNerdHQ <codenerdhq@gmail.com>
2023-04-17 08:24:09 +0100

    fixed typo

--- AnimationTutorial.swift
+++ AnimationTutorial.swift
@@ -1,6 +1,6 @@
 // Tutorial built on the activities inside the Book:
 // "SwiftUI Cookbook - Second Edition"
-// The book uses Swift 3, Swift 5.5
+// The book uses SwiftUI 3, Swift 5.5
 import SwiftUI
 
 struct ContentView: View {
